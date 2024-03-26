681358bf320a8b0a0c0a0f23ebedc0b833b8c666
Utkarsh Sharma <utkarsh.sharma@novoinvent.com>
2024-03-26 05:42:32 +0530

    Reference Type DI with P schema

--- src/main/java/ReferenceType/Test.java
+++ src/main/java/ReferenceType/Test.java
@@ -7,6 +7,8 @@ public class Test {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("RefConfig.xml");
         A a = (A)context.getBean("a");
+        A a2 = (A)context.getBean("a2");
         System.out.println(a);
+        System.out.println(a2);
     }
 }
